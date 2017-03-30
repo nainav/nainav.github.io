bc19637986fa5503033a4a7e5a3088ad134ef966
nainav <naina.verma7@gmail.com>
2017-03-30 23:22:43 +0530

    Add

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var pkg = require('./package.json');
 // Set the banner content
 var banner = ['/*!\n',
     ' * Naina - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2009-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * Copyright 2009 -' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
     ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
     ' */\n',
     ''
